f7c915eb2937db42649ee0d89d0384a870d12743
James Ferguson <wjcferguson@gmail.com>
2018-03-02 19:25:39 -0500

    version bump

--- faff-theme.el
+++ faff-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: James Ferguson <(concat "wjcferguson" at-sign "gmail.com")>
 ;; URL: https://github.com/WJCFerguson/emacs-faff-theme
-;; Version: 20170522.13:20
+;; Version: 20180302.19:25
 ;; Keywords: color theme
 
 ;; This file is not part of GNU Emacs.
