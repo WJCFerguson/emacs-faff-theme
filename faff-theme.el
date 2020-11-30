595d29a1b9230e65efaa8c7cf02690050db63306
James Ferguson <wjcferguson@gmail.com>
2020-11-30 11:53:24 -0500

    make hydra pink face more readable

--- faff-theme.el
+++ faff-theme.el
@@ -143,6 +143,7 @@
  '(helm-ff-symlink ((t (:foreground "orange4"))))
  '(helm-grep-lineno ((t (:foreground "orange4"))))
  '(helm-source-header ((t (:background "cornsilk2" :foreground "black" :box (:line-width 1 :color "grey75" :style pressed-button) :weight normal :height 1.0 :family "Sans Serif"))))
+ '(hydra-face-pink ((t (:foreground "deeppink" :weight bold))))
  '(Info-quoted ((t (:inherit fixed-pitch))))
  '(magit-branch ((t (:inherit magit-header :background "yellow" :box (:line-width 1 :color "grey75" :style released-button)))))
  '(magit-branch-current ((t (:inherit magit-branch-local :background "green1" :weight bold))))
