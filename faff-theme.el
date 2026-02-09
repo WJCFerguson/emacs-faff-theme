5c063e34db8eb7a79d0283fde2a01281e99ef2c3
James Ferguson <james@faff.org>
2026-02-09 15:51:22 -0500

    v3.9

--- faff-theme.el
+++ faff-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: James Ferguson <(concat "wjcferguson" at-sign "gmail.com")>
 ;; URL: https://github.com/WJCFerguson/emacs-faff-theme
-;; Version: 3.8
+;; Version: 3.9
 ;; Keywords: color theme
 
 ;; This file is not part of GNU Emacs.
