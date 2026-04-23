20795bbbefdc765b2961f0cae2f4cce5f06bd179
James Ferguson <james@faff.org>
2026-04-23 17:09:03 -0400

    separate error and warning more

--- faff-theme.el
+++ faff-theme.el
@@ -143,8 +143,8 @@
     (bg-region "#e0dcc8")))
 
 (defconst faff-palette-mappings-partial
-  '((err red-warmer)
-    (warning yellow-warmer)
+  '((err red-cooler)
+    (warning yellow-faint)
     (info green)
 
     (fg-link blue-warmer)
